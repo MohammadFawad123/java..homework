5bd338c0df5e9f4ad976fdce297c7f91f924e6cd
MohammadFawad123 <jannatimohammadfawad@gmail.com>
2025-11-16 06:53:24 +0430

    1

---
+++ SmallSquare.java
@@ -0,0 +1,16 @@
+
+package small.square;
+
+public class SmallSquare {
+
+    public static void main(String[] args) {
+          int n = 1;
+        
+        while((n*n) <= 12000){
+            n++;
+        }
+        
+        System.out.println("Result is " + n);
+    }
+    
+}
---
+++ SumOfDigit.java
@@ -0,0 +1,20 @@
+
+package sum.of.digit;
+import java.util.Scanner;
+public class SumOfDigit {
+
+    public static void main(String[] args) {
+       Scanner input = new Scanner(System.in);
+         System.out.println(sumDigit(344));
+    }
+    
+    public static int sumDigit(int digit){
+        int dig1 = digit / 100;
+        int dig2 = (digit/10) % 10 ;
+        int dig3 = (digit % 100) % 10;
+        
+        return dig1 + dig2 + dig3;
+       
+    }
+    
+}
